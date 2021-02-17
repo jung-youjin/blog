6d7a286687bc96e21f3a945b7a682a8ad96f68c0
YouJin Jung <jungyoujin0527@gmail.com>
2021-02-17 23:04:41 +0900

    add embeddedsw tag

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import IndexLayout from '../layouts';
 import Wrapper from '../components/Wrapper';
 import SiteNav from '../components/header/SiteNav';
@@ -78,68 +77,3 @@ const About: React.FC = () => (
 );
 
 export default About;
-=======
-import IndexLayout from '../layouts';
-import Wrapper from '../components/Wrapper';
-import SiteNav from '../components/header/SiteNav';
-import { SiteHeader, outer, inner, SiteMain } from '../styles/shared';
-import * as React from 'react';
-import { css } from '@emotion/core';
-
-import { PostFullHeader, PostFullTitle, NoImage, PostFull } from '../templates/post';
-import { PostFullContent } from '../components/PostContent';
-import Footer from '../components/Footer';
-import Helmet from 'react-helmet';
-
-const PageTemplate = css`
-  .site-main {
-    background: #fff;
-    padding-bottom: 4vw;
-    font-family: Consolas;
-  }
-  h3, p {
-    font-family: Consolas;
-  }
-
-`;
-
-const About_title = css`
-  title {
-    font-family: Consolas;
-  }
-`;
-
-
-
-const About: React.FC = () => (
-  <IndexLayout>
-    <Helmet>
-      <title css={About_title}>About</title>
-    </Helmet>
-    <Wrapper css={PageTemplate}>
-      <header css={[outer, SiteHeader]}>
-        <div css={inner}>
-          <SiteNav />
-        </div>
-      </header>
-      <main id="site-main" className="site-main" css={[SiteMain, outer]}>
-        <article className="post page" css={[PostFull, NoImage]}>
-          <PostFullHeader>
-            <PostFullTitle>About</PostFullTitle>
-          </PostFullHeader>
-
-          <PostFullContent className="post-full-content">
-            <div className="post-content">
-              <h3> youjin's blog - since 2020.03.20 ~ing </h3>
-              <small><p> Welcome to my blog! Further contents to be updated...! Stay tuned! </p></small>
-            </div>
-          </PostFullContent>
-        </article>
-      </main>
-      <Footer />
-    </Wrapper>
-  </IndexLayout>
-);
-
-export default About;
->>>>>>> parent of 0fa3283... add portfolio images
